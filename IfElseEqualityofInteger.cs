f24a581f2c766aa8676426b8ddc00ef8c846b5b4
Snehasrg <sghongade642@gmail.com>
2023-03-16 20:11:37 +0530

    Solved using If Else

---
+++ IfElseEqualityofInteger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    public class IfElseEqualityofInteger
+    {
+        public static void CheckEquality()
+        {
+            Console.WriteLine("Enter first integer");
+            int I1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second integer");
+            int I2 = Convert.ToInt32(Console.ReadLine());
+            if (I1 == I2)
+            {
+                Console.WriteLine("Two integers are equal");
+            }
+            else
+            {
+                Console.WriteLine("Two integers are not equal");
+            }
+        }
+    }
+}
---
+++ IfElseVotingAge.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    public class IfElseVotingAge
+    {
+        public static void CheckEligibility()
+        {
+            Console.WriteLine("Enter the age of candidate");
+            int Age = Convert.ToInt32(Console.ReadLine());
+            if (Age >= 18)
+            {
+                Console.WriteLine("Candidate is eligible for voting.");
+            }
+            else
+            {
+                Console.WriteLine("Candidate is not eligible for voting.");
+            }
+        }
+    }
+}
